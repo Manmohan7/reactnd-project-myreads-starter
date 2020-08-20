6f302a3f644f4858ca8e23f6640a092d5619b02b
Manmohan7 <er.manmohansingh7@gmail.com>
2020-08-20 07:09:38 +0530

    refactor: ShelfChanger to functional component

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 
-class ShelfChanger extends Component {
-  static propTypes = {
-    currentShelf: PropTypes.string.isRequired,
-    updateShelf: PropTypes.func.isRequired
-  }
+function ShelfChanger(props) {
+  const { currentShelf, updateShelf } = props
 
-  render() {
-    const { currentShelf, updateShelf } = this.props
+  return (
+    <div className="book-shelf-changer">
+      <select value={currentShelf} onChange={(event) => updateShelf(event.target.value)}>
+        <option value="move" disabled>Move to...</option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  )
+}
 
-    return (
-      <div className="book-shelf-changer">
-        <select value={currentShelf} onChange={(event) => updateShelf(event.target.value)}>
-          <option value="move" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    )
-  }
-
+ShelfChanger.propTypes = {
+  currentShelf: PropTypes.string.isRequired,
+  updateShelf: PropTypes.func.isRequired
 }
 
 export default ShelfChanger
